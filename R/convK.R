ff064b5164e135c863f567027a780f1c18c901ab
Nicola <nicola.dinapoli.74@gmail.com>
2015-09-09 22:18:18 +0200

    sobel

--- R/convK.R
+++ R/convK.R
@@ -2,22 +2,24 @@
 #' @description This function creates the convolution kernel for applying a filter to an array/matrix
 #' @param sigma The \code{numeric} value of standard deviation for the Gaussian or LoG filter
 #' @param k \code{character} value: \code{gaussian} for Gaussian kernel, \code{LoG} for Laplacian of Gaussian kernel, \code{sharpen} for
-#' 3x3 convolution matrix for sharping the edges, \code{laplacian} for a 3x3 convolution matrix that enhance the edges and \code{emboss} for a 3x3 kernel that
-#' draws edges as embossed image
+#' 3x3 convolution matrix for sharpening edges, \code{laplacian} for a 3x3 convolution matrix that enhance the edges and \code{emboss} for a 3x3 kernel that
+#' draws edges as embossed image, \code{sobel} gives only one of the two matrices needed to apply the Sobel filter
 #' @details The convolution kernel is a matrix that is used by \code{spacialfil} function over a matrix, or array, for filtering
 #' the data. \emph{Gaussian}  kernel is calculated starting from the 2 dimension, isotropic, Gaussian distribution:
 #' \deqn{G(x)=\frac{1}{2\pi\sigma^{2}}e^{-\frac{x^{2}+y^{2}}{2\sigma^{2}}}} \emph{Laplacian of Gaussian} kernel applies
 #' a second derivative to enhance regions of rapid intensity changes:
 #' \deqn{LoG\left ( x,y \right )=\frac{-1}{\pi\sigma^{4}}\left ( 1-\frac{x^{2}+y^{2}}{2\sigma^{2}}\right ) e^{-\frac{x^{2}+y^{2}}{2\sigma^{2}}}} the use of the underlying Gaussian kernel (so the name
 #' Laplacian of Gaussian or \emph{LoG}) is needed to reduce the effect of high frequency noise that can affect the signal
-#' distribution. \emph{Edge} kernel is a 3x3 cnvolution kernel used to enhance the edges of the matrix. \emph{Sharpen} enhance the detail
-#' (but also the noise) in original dataset
+#' distribution. \emph{Laplacian} is a \emph{Sharpen} enhance the detail. \emph{Emboss} kernel is a 3x3 convolution kernel that embosses the edges. 
+#' (but also the noise) in original dataset. \emph{Sobel} convolution kernel returns the possibility to detect edges in a more sofisticated
+#' way, the \code{convKernel} function returns only one of the two matrices need to apply the filter. The second one is calculated
+#' by transposing the returned matrix in the other needed one.
 #' @return An object of class \code{convKern} with the \code{matrix} of convolution kernel whose size varies according the value of \code{sigma} (in case of
-#' \code{gaussian} or \code{LoG} option selected), and \code(k) being the convolution kernel type label
+#' \code{gaussian} or \code{LoG} option selected), and \code{k} being the convolution kernel type label
 #' @export
 #' @examples # creates a convolution kernel with Gaussian function and sigma = 1.4
 #'  K <- convKernel(sigma = 1.4, k = 'gaussian')
-convKernel <- function(sigma = 1.4, k = c('gaussian','LoG','sharpen','laplacian','emboss')) {
+convKernel <- function(sigma = 1.4, k = c('gaussian','LoG','sharpen','laplacian','emboss','sobel')) {
   k <- match.arg(k)
   l <- sigma * 7
   # check if odd and if not increas by one
@@ -31,9 +33,10 @@ convKernel <- function(sigma = 1.4, k = c('gaussian','LoG','sharpen','laplacian'
   if (k=='LoG')
     M <- outer(X = x, Y = y, FUN = function(X, Y) return(-1/(pi*sigma^4)*(1-(X^2+Y^2)/(2*sigma^2))*exp(-(X^2+Y^2)/(2*sigma^2))))
 
-  if (k=='sharpen') M <- matrix(data = c(0,-1,0,-1,5,-1,0,-1,0), nrow = 3)
+  if (k=='sharpen')   M <- matrix(data = c(0,-1,0,-1,5,-1,0,-1,0), nrow = 3)
   if (k=='laplacian') M <- matrix(data = c(.5,1,.5,1,-6,1,.5,1,.5), nrow = 3)
-  if (k=='emboss') M <- matrix(c(-1,0,1,-1,0,1,-1,0,1), nrow = 3)
+  if (k=='emboss')    M <- matrix(c(-1,0,1,-1,0,1,-1,0,1), nrow = 3)
+  if (k=='sobel')     M <- matrix(c(1,2,1,0,0,0,-1,-2,-1), nrow = 3)
   # create S3 class
   output <- list('matrix' = M, 'kernel' = k)
   class(output) <- 'convKern'
@@ -79,19 +82,39 @@ applyFilter <- function(x, kernel) {
   for (n in -extralines:extralines)  # index for columns
     for (m in -extralines:extralines)  # index for rows
       kindex <- c(kindex, n*Nrow + m)
-  if ((kernel$kernel == 'laplacian') || (kernel$kernel == 'emboss'))
+  # apply laplacian and emboss convolution kernels
+  if ((kernel$kernel == 'laplacian') || (kernel$kernel == 'emboss')) {
     result <- .C('applyKernelWithoutNorm', as.double(x), as.double(kernel$matrix), as.integer(extralines), as.integer(kindex),
                  as.integer(Nrow), as.integer(Ncol), as.integer(Nslices), as.double(dataOutput))
-  else
+    result <- result[[8]]
+    }
+  # apply Gaussian, LoG and sharpen convolution kernels
+  else if (kernel$kernel != 'sobel')  {# apply Gaussian, LoG and sharpen convolution kernels
     result <- .C('applyKernel', as.double(x), as.double(kernel$matrix), as.integer(extralines), as.integer(kindex),
                  as.integer(Nrow), as.integer(Ncol), as.integer(Nslices), as.double(dataOutput))
+    result <- result[[8]]
+    }
+  # apply Sobel convolution kernels
+  if (kernel$kernel == 'sobel') {
+    Gx <- .C('applyKernelWithoutNorm', as.double(x), as.double(kernel$matrix), as.integer(extralines), as.integer(kindex),
+             as.integer(Nrow), as.integer(Ncol), as.integer(Nslices), as.double(dataOutput))
+    Gx <- Gx[[8]]
+    dataOutput <- x
+    # Gy convolution kernel is given by transposition of sobel default convolution kernel 
+    Gy <- .C('applyKernelWithoutNorm', as.double(x), as.double(t(kernel$matrix)), as.integer(extralines), as.integer(kindex),
+             as.integer(Nrow), as.integer(Ncol), as.integer(Nslices), as.double(dataOutput))
+    Gy <- Gy[[8]]
+    result <- sqrt(Gx^2 + Gy^2)
+  }
+  
+  
   if (class(x)=='matrix') {
-    output <- matrix(data = result[[8]], nrow = Nrow)
+    output <- matrix(data = result, nrow = Nrow)
     # resize matrix to original size
     output <- output[(extralines+1):(nrow(output) - extralines), (extralines+1):(ncol(output) - extralines)]
   }
   if (class(x)=='array')  {
-    output <- array(data = result[[8]], dim = c(Nrow, Ncol, Nslices))
+    output <- array(data = result, dim = c(Nrow, Ncol, Nslices))
     output <- output[(extralines+1):(nrow(output) - extralines), (extralines+1):(ncol(output) - extralines),]
   }
   return(output)
